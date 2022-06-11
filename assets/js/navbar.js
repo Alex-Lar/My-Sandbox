cc640ae3831316af79bb09f632f50c6c8dc85647
Alexander Larin <doremilarin@gmail.com>
2022-06-11 10:13:32 +0300

    added radio buttons

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,4 +1,24 @@
 const burgerBtn = document.querySelector('.hamburger');
+const [redBtn, blueBtn] = document.querySelectorAll('.radio-item');
+
+redBtn.addEventListener('click', function() {
+    if (!redBtn.classList.contains('red')) {
+        redBtn.classList.add('red');
+    }
+    if (blueBtn.classList.contains('blue')) {
+        blueBtn.classList.remove('blue');
+    }
+});
+
+blueBtn.addEventListener('click', function() {
+    if (!blueBtn.classList.contains('blue')) {
+        blueBtn.classList.add('blue');
+    }
+    if (redBtn.classList.contains('red')) {
+        redBtn.classList.remove('red');
+    }
+});
+
 
 burgerBtn.addEventListener('click', function() {
     const navMenu = document.querySelector('.nav-menu');
